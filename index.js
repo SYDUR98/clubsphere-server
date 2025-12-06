c5519095743d4599c641f91ed57a9d6411f5e220
sydur <eng.sydur@gmail.com>
2025-12-06 15:31:15 +0600

    added middleware cors

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const cors = require("cors");
 const app = express()
 const port = 3000
+
+// Middleware
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
